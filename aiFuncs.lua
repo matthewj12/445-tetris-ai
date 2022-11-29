bc9379d6c3548f5c1987cfbe96b4fe7b58c32e1f
Rickey Marshally <80717255+Marsh1997@users.noreply.github.com>
2022-11-28 20:13:08 -0600

    Flatness function
    Calculates the flatness based on the chosen algorithm.

--- aiFuncs.lua
+++ aiFuncs.lua
@@ -185,6 +185,45 @@ function countHoles(the_pf)
 	return hole_count
 end
 
+-- returns the flatness number of the current playfield. 
+-- flatness is determined by taking the absolute difference between the current and last column
+function flatness(the_pf)
+	local pf = the_pf
+	local flatness_count = 0
+
+	-- need to make the last flatness -1 because we havent started. 
+	-- -1 will also be used to identify the first column so we can skip it. 
+	local current_flatness = 0
+	local last_flatness = -1
+
+	-- going backwards starting from the top of the playfield
+	for x = 10, 1, -1 do
+
+		for y = 20, 1, -1 do
+			if pf[y][x] == 1 then
+				-- once we get to the first block coming down from the top,
+				-- the current index is basically the height of the current column
+				current_flatness = x
+				-- break here because we have reach the top of the column where a block can be placed. 
+				break
+			end
+		end
+
+		-- get the abolute difference
+		local diff = math.abs(current_flatness - last_flatness)
+		-- as long as diff is greater than or equal to 3 and last flatness is not -1
+		if diff >= 3 and last_flatness != -1 then
+			-- add the flatness since this abs diff meets our criteria
+			flatness_count = flatness_count + diff
+		end
+
+		-- current flatness is now last flatness because we are going to the next number
+		last_flatness = current_flatness
+	end
+
+	return flatness_count
+end
+
 
 -- applies the given move for the given tetromino to the given playfield and returns the resulting playfield
 
